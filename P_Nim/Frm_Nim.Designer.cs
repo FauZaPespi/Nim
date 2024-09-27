d7d865b0b9e02a7a2b85ba5abc92f5c312e4102b
FauZa <160544435+FauZaPespi@users.noreply.github.com>
2024-09-27 10:11:30 +0200

    Add files via upload

---
+++ P_Nim/Frm_Nim.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace P_Nim
+{
+    partial class Frm_Nim
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbJ1 = new System.Windows.Forms.GroupBox();
+            this.btnJ1A3 = new System.Windows.Forms.Button();
+            this.btnJ1A2 = new System.Windows.Forms.Button();
+            this.btnJ1A1 = new System.Windows.Forms.Button();
+            this.grbJ2 = new System.Windows.Forms.GroupBox();
+            this.btnJ2A3 = new System.Windows.Forms.Button();
+            this.btnJ2A1 = new System.Windows.Forms.Button();
+            this.btnJ2A2 = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnLeave = new System.Windows.Forms.Button();
+            this.lblTour = new System.Windows.Forms.Label();
+            this.lblAllumetteStill = new System.Windows.Forms.Label();
+            this.pnlAllumettes = new System.Windows.Forms.Panel();
+            this.grbJ1.SuspendLayout();
+            this.grbJ2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbJ1
+            // 
+            this.grbJ1.Controls.Add(this.btnJ1A3);
+            this.grbJ1.Controls.Add(this.btnJ1A2);
+            this.grbJ1.Controls.Add(this.btnJ1A1);
+            this.grbJ1.Location = new System.Drawing.Point(13, 240);
+            this.grbJ1.Name = "grbJ1";
+            this.grbJ1.Size = new System.Drawing.Size(217, 163);
+            this.grbJ1.TabIndex = 3;
+            this.grbJ1.TabStop = false;
+            this.grbJ1.Text = "Joueur 1:";
+            // 
+            // btnJ1A3
+            // 
+            this.btnJ1A3.Location = new System.Drawing.Point(21, 110);
+            this.btnJ1A3.Name = "btnJ1A3";
+            this.btnJ1A3.Size = new System.Drawing.Size(174, 23);
+            this.btnJ1A3.TabIndex = 2;
+            this.btnJ1A3.Text = "trois allumette";
+            this.btnJ1A3.UseVisualStyleBackColor = true;
+            this.btnJ1A3.Click += new System.EventHandler(this.btnJ1A3_Click);
+            // 
+            // btnJ1A2
+            // 
+            this.btnJ1A2.Location = new System.Drawing.Point(21, 70);
+            this.btnJ1A2.Name = "btnJ1A2";
+            this.btnJ1A2.Size = new System.Drawing.Size(174, 23);
+            this.btnJ1A2.TabIndex = 1;
+            this.btnJ1A2.Text = "deux allumette";
+            this.btnJ1A2.UseVisualStyleBackColor = true;
+            this.btnJ1A2.Click += new System.EventHandler(this.btnJ1A2_Click);
+            // 
+            // btnJ1A1
+            // 
+            this.btnJ1A1.Location = new System.Drawing.Point(21, 30);
+            this.btnJ1A1.Name = "btnJ1A1";
+            this.btnJ1A1.Size = new System.Drawing.Size(174, 23);
+            this.btnJ1A1.TabIndex = 0;
+            this.btnJ1A1.Text = "une allumette";
+            this.btnJ1A1.UseVisualStyleBackColor = true;
+            this.btnJ1A1.Click += new System.EventHandler(this.btnJ1A1_Click);
+            // 
+            // grbJ2
+            // 
+            this.grbJ2.Controls.Add(this.btnJ2A3);
+            this.grbJ2.Controls.Add(this.btnJ2A1);
+            this.grbJ2.Controls.Add(this.btnJ2A2);
+            this.grbJ2.Location = new System.Drawing.Point(571, 240);
+            this.grbJ2.Name = "grbJ2";
+            this.grbJ2.Size = new System.Drawing.Size(217, 163);
+            this.grbJ2.TabIndex = 3;
+            this.grbJ2.TabStop = false;
+            this.grbJ2.Text = "Joueur 2:";
+            // 
+            // btnJ2A3
+            // 
+            this.btnJ2A3.Enabled = false;
+            this.btnJ2A3.Location = new System.Drawing.Point(21, 110);
+            this.btnJ2A3.Name = "btnJ2A3";
+            this.btnJ2A3.Size = new System.Drawing.Size(174, 23);
+            this.btnJ2A3.TabIndex = 2;
+            this.btnJ2A3.Text = "trois allumette";
+            this.btnJ2A3.UseVisualStyleBackColor = true;
+            this.btnJ2A3.Click += new System.EventHandler(this.btnJ2A3_Click);
+            // 
+            // btnJ2A1
+            // 
+            this.btnJ2A1.Enabled = false;
+            this.btnJ2A1.Location = new System.Drawing.Point(21, 30);
+            this.btnJ2A1.Name = "btnJ2A1";
+            this.btnJ2A1.Size = new System.Drawing.Size(174, 23);
+            this.btnJ2A1.TabIndex = 0;
+            this.btnJ2A1.Text = "une allumette";
+            this.btnJ2A1.UseVisualStyleBackColor = true;
+            this.btnJ2A1.Click += new System.EventHandler(this.btnJ2A1_Click);
+            // 
+            // btnJ2A2
+            // 
+            this.btnJ2A2.Enabled = false;
+            this.btnJ2A2.Location = new System.Drawing.Point(21, 70);
+            this.btnJ2A2.Name = "btnJ2A2";
+            this.btnJ2A2.Size = new System.Drawing.Size(174, 23);
+            this.btnJ2A2.TabIndex = 1;
+            this.btnJ2A2.Text = "deux allumette";
+            this.btnJ2A2.UseVisualStyleBackColor = true;
+            this.btnJ2A2.Click += new System.EventHandler(this.btnJ2A2_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(314, 307);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(174, 23);
+            this.btnNewGame.TabIndex = 4;
+            this.btnNewGame.Text = "Nouvelle partie";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnLeave
+            // 
+            this.btnLeave.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnLeave.Location = new System.Drawing.Point(314, 347);
+            this.btnLeave.Name = "btnLeave";
+            this.btnLeave.Size = new System.Drawing.Size(174, 23);
+            this.btnLeave.TabIndex = 5;
+            this.btnLeave.Text = "Quitter";
+            this.btnLeave.UseVisualStyleBackColor = true;
+            this.btnLeave.Click += new System.EventHandler(this.btnLeave_Click);
+            // 
+            // lblTour
+            // 
+            this.lblTour.AutoSize = true;
+            this.lblTour.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTour.Location = new System.Drawing.Point(342, 225);
+            this.lblTour.Name = "lblTour";
+            this.lblTour.Size = new System.Drawing.Size(116, 15);
+            this.lblTour.TabIndex = 2;
+            this.lblTour.Text = "Tour du Joueur 1";
+            // 
+            // lblAllumetteStill
+            // 
+            this.lblAllumetteStill.AutoSize = true;
+            this.lblAllumetteStill.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAllumetteStill.Location = new System.Drawing.Point(335, 162);
+            this.lblAllumetteStill.Name = "lblAllumetteStill";
+            this.lblAllumetteStill.Size = new System.Drawing.Size(137, 16);
+            this.lblAllumetteStill.TabIndex = 1;
+            this.lblAllumetteStill.Text = "Allumettes restante 20";
+            this.lblAllumetteStill.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlAllumettes
+            // 
+            this.pnlAllumettes.Location = new System.Drawing.Point(12, 12);
+            this.pnlAllumettes.Name = "pnlAllumettes";
+            this.pnlAllumettes.Size = new System.Drawing.Size(776, 147);
+            this.pnlAllumettes.TabIndex = 6;
+            // 
+            // Frm_Nim
+            // 
+            this.AcceptButton = this.btnNewGame;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnLeave;
+            this.ClientSize = new System.Drawing.Size(800, 413);
+            this.Controls.Add(this.lblAllumetteStill);
+            this.Controls.Add(this.lblTour);
+            this.Controls.Add(this.btnLeave);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.grbJ2);
+            this.Controls.Add(this.grbJ1);
+            this.Controls.Add(this.pnlAllumettes);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 452);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(816, 452);
+            this.Name = "Frm_Nim";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jeux du Nim";
+            this.Load += new System.EventHandler(this.Frm_Nim_Load);
+            this.grbJ1.ResumeLayout(false);
+            this.grbJ2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox grbJ1;
+        private System.Windows.Forms.GroupBox grbJ2;
+        private System.Windows.Forms.Button btnJ1A3;
+        private System.Windows.Forms.Button btnJ1A2;
+        private System.Windows.Forms.Button btnJ1A1;
+        private System.Windows.Forms.Button btnJ2A3;
+        private System.Windows.Forms.Button btnJ2A1;
+        private System.Windows.Forms.Button btnJ2A2;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnLeave;
+        private System.Windows.Forms.Label lblTour;
+        private System.Windows.Forms.Label lblAllumetteStill;
+        private System.Windows.Forms.Panel pnlAllumettes;
+    }
+}
+
---
+++ P_Nim/Frm_Nim.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace P_Nim
+{
+    /*
+     * Auteur: FauZaPespi
+     * Date: 27.09.2024
+     * Détails: Projet qui permet à deux joueurs (humains) de jouer au jeu des allumettes. (Nim)
+     */
+    public partial class Frm_Nim : Form
+    {
+        // Initialisation des variables
+
+
+        //Constante du nombre max d'allumettes possible dans le jeux
+        const int _nbMaxAllumette = 20;
+
+        private bool _running = false;
+        private bool _tour = true;
+        private int nbActuelleAllumette = _nbMaxAllumette;
+        private List<PictureBox> pictureBoxes = new List<PictureBox>();
+
+        /// <summary>
+        /// Constructeur de class
+        /// </summary>
+        public Frm_Nim()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Dans cette methode on entre nbAllumettes, elle va crée et ajouter des la liste des picturebox avec les images correctes dedans
+        /// </summary>
+        /// <param name="nbAllumettes">nombre d'allumettes a générée</param>
+        private void DrawAllumettes(int nbAllumettes)
+        {
+            int lastPos = 3;
+            for (int i = 0; i < nbAllumettes; i++)
+            {
+                PictureBox picture = new PictureBox
+                {
+                    Name = $"pb{i}",
+                    Size = new System.Drawing.Size(41, 141),
+                    SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom,
+                    Location = new System.Drawing.Point(lastPos, 3),
+                    Image = P_Nim.Properties.Resources.allumette,
+                };
+                lastPos += 38;
+                pnlAllumettes.Controls.Add(picture);
+                pictureBoxes.Add(picture);
+            }
+            lblAllumetteStill.Text = $"Allumettes restante {pictureBoxes.Count}";
+        }
+
+        /// <summary>
+        /// Methode pour quitter l'application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLeave_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /// <summary>
+        /// Lance la partie ou non selon la condition !_running
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            if (!_running)
+            {
+                DrawAllumettes(_nbMaxAllumette);
+                btnNewGame.Enabled = false;
+                stateGrb(grbJ1, true);
+                stateGrb(grbJ2, false);
+                _tour = true;
+                _running = true;
+            }
+            else
+            {
+                btnNewGame.Enabled = true;
+                stateGrb(grbJ1, false);
+                stateGrb(grbJ2, false);
+            }
+        }
+        /// <summary>
+        /// Méthod pour "réinitialiser" tous le jeux
+        /// </summary>
+        private void RestartGame()
+        {
+            ClearAllumettes();
+            nbActuelleAllumette = _nbMaxAllumette;
+            lblAllumetteStill.Text = "Allumettes restante 20";
+            lblTour.Text = "Tour du Joueur 1";
+            _running = false;
+            _tour = true;
+            pnlAllumettes.Controls.Clear();
+            stateGrb(grbJ1, false);
+            stateGrb(grbJ2, false);
+            btnNewGame.Enabled = true;
+        }
+        /// <summary>
+        /// Au chargement du formulaire lancement de la permière partie
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Frm_Nim_Load(object sender, EventArgs e)
+        {
+            if (!_running)
+            {
+                DrawAllumettes(_nbMaxAllumette);
+                btnNewGame.Enabled = false;
+                _running = true;
+            }
+            else
+            {
+                btnNewGame.Enabled = true;
+            }
+        }
+        /// <summary>
+        /// Méthode pour supprimé toute les allumettes
+        /// </summary>
+        private void ClearAllumettes()
+        {
+            pnlAllumettes.Controls.Clear();
+            pictureBoxes.Clear();
+        }
+        /// <summary>
+        /// Affichage du gagnant
+        /// </summary>
+        /// <param name="joueur"></param>
+        private void WinGame(bool joueur)
+        {
+            MessageBox.Show($"Joueur {(joueur ? "Joueur 2" : "Joueur 1")} à gagner", "Bravo");
+            ClearAllumettes();
+            RestartGame();
+        }
+        /// <summary>
+        /// Méthode pour enlever le nombre d'allumette voulu
+        /// </summary>
+        /// <param name="nbRemove">nombre d'allumettes a enlever</param>
+        /// <param name="joueur">joueur qui a enlever l'allumettes</param>
+        private void RemoveAllumettes(int nbRemove, bool joueur)
+        {
+            ClearAllumettes();
+            nbActuelleAllumette -= nbRemove;
+            DrawAllumettes(nbActuelleAllumette);
+            if (joueur)
+            {
+                stateGrb(grbJ1, false);
+                stateGrb(grbJ2, true);
+            }
+            else
+            {
+                stateGrb(grbJ1, true);
+                stateGrb(grbJ2, false);
+            }
+            lblTour.Text = joueur ? "Tour du Joueur 2" : "Tour du Joueur 1";
+            if (pictureBoxes.Count < 3)
+            {
+                btnJ1A3.Enabled = false;
+                btnJ2A3.Enabled = false;
+            }
+            if (pictureBoxes.Count < 2)
+            {
+                btnJ1A2.Enabled = false;
+                btnJ2A2.Enabled = false;
+            }
+            if (pictureBoxes.Count < 1)
+            {
+                WinGame(joueur);
+                return;
+            }
+            _tour = !_tour;
+
+        }
+        /// <summary>
+        /// Change l'etat de tout les bouttons dans la groupbox
+        /// </summary>
+        /// <param name="groupbox"></param>
+        /// <param name="state"></param>
+        private void stateGrb(GroupBox groupbox, bool state)
+        {
+            try
+            {
+                foreach (Button button in groupbox.Controls)
+                {
+                    button.Enabled = state;
+                }
+            }
+            catch { }
+        }
+        /// <summary>
+        /// Methode de click correspondant
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnJ1A1_Click(object sender, EventArgs e)
+        {
+            if (_tour)
+            {
+                RemoveAllumettes(1, true);
+                stateGrb((GroupBox)btnJ1A1.Parent, false);
+            }
+        }
+        /// <summary>
+        /// Methode de click correspondant
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnJ1A2_Click(object sender, EventArgs e)
+        {
+            if (_tour)
+            {
+                RemoveAllumettes(2, true);
+                stateGrb((GroupBox)btnJ1A2.Parent, false);
+            }
+        }
+        /// <summary>
+        /// Methode de click correspondant
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnJ1A3_Click(object sender, EventArgs e)
+        {
+            if (_tour)
+            {
+                RemoveAllumettes(3, true);
+                stateGrb((GroupBox)btnJ1A3.Parent, false);
+            }
+        }
+        /// <summary>
+        /// Methode de click correspondant
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnJ2A1_Click(object sender, EventArgs e)
+        {
+            if (!_tour)
+            {
+                RemoveAllumettes(1, false);
+                stateGrb((GroupBox)btnJ2A1.Parent, false);
+            }
+        }
+        /// <summary>
+        /// Methode de click correspondant
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnJ2A2_Click(object sender, EventArgs e)
+        {
+            if (!_tour)
+            {
+                RemoveAllumettes(2, false);
+                stateGrb((GroupBox)btnJ2A2.Parent, false);
+            }
+        }
+        /// <summary>
+        /// Methode de click correspondant
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnJ2A3_Click(object sender, EventArgs e)
+        {
+            if (!_tour)
+            {
+                RemoveAllumettes(3, false);
+                stateGrb((GroupBox)btnJ2A3.Parent, false);
+            }
+        }
+    }
+}
